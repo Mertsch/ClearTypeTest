d292adc4915ab975f4d92a20ebb54c8be239ce86
Mertsch <Mertsch@users.noreply.github.com>
2017-02-05 14:07:11 +0100

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Windows;
+
+namespace ClearTypeTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
