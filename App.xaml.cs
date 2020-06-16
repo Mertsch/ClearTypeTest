4f7c89d092943dcc2a594bbc8fd3619258ce6091
Martin <martin@freedom-manufaktur.com>
2020-06-16 08:43:14 +0200

    Cleanup the source, make it more apparent what's the purpose of this app

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
 using System.Windows;
 
 namespace ClearTypeTest
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+    public partial class App : Application { }
 }
